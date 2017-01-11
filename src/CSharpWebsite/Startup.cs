da3870ffe2c908359dd883df1a5efee8d254ebbf
Stephen Wiggins <steve.wiggins7@gmail.com>
2017-01-11 03:35:48 -0500

    add resume and css to site

--- src/CSharpWebsite/Startup.cs
+++ src/CSharpWebsite/Startup.cs
@@ -41,7 +41,7 @@ namespace CSharpWebsite
         {
             // Add framework services.
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("LinuxServer")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
