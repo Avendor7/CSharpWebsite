669aa0715b3bf590aca2f1cbeec21e1f51b574ce
Stephen Wiggins <steve.wiggins7@gmail.com>
2017-01-13 03:45:45 -0500

    pre-db reverse engineer

---
+++ tableCreate.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE TABLE [IPAddress] (
+    [ID] int NOT NULL IDENTITY,
+    [ipv4] varchar(21),
+    [ipv6] varchar(45),
+	[vm] bit NOT NULL,
+	[LXC_container] bit NOT NULL,
+	[docker] bit NOT NULL,
+	[operating_system] varchar(20) NOT NULL,
+	[description] text NOT NULL,
+);
+GO
+
+INSERT INTO IPAddress values ('192.168.5.143:32400',null,0,1,0,'Ubuntu','Plex Server');
+GO
+
+SELECT * FROM [IPAddress];
